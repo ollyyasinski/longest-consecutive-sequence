d0c8988a9950cff3ca4f57053b9c602638879029
Yasinskaya, Volha <volha.yasinskaya@kronos.com>
2018-03-02 12:06:56 +0300

    Feature/longest-consecutive-sequence: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+module.exports =
+  function longestConsecutiveLength(array) {
+    var set = new Set(array);
+    var count = 0;
+
+    set.forEach(function(i) {
+      if (!set.has(i - 1)) {
+        var currentI = i;
+        var tempCount = 1;
+        while (set.has(currentI + 1)) {
+          currentI += 1;
+          tempCount += 1;
+        }
+        count = Math.max(count, tempCount);
+      }
+    }
+    )
+    return count;
+  }
